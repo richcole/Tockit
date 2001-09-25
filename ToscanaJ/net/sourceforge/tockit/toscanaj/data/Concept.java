19dd26dcfd3f6f8075e42aa03905980e382f32c1
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2001-09-25 02:29:22 +0000

    Added interface for Concepts.
    This is a pretty simple abstraction layer for the concepts, forthcoming
    implementations will map this either to sets hold in memory or to SQL
    queries.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@36 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ ToscanaJ/net/sourceforge/tockit/toscanaj/data/Concept.java
@@ -0,0 +1,83 @@
+package net.sourceforge.tockit.toscanaj.data;
+
+import java.util.Iterator;
+
+/**
+ * Offers read access to information about a concept in a Formal Concept
+ * Lattice.
+ *
+ * This does model the intent, the extent and the two contingents (attribute and
+ * object) by giving access to the size information and an iterator for each. In
+ * addition a query can be made if this concept is realized, thereby allowing
+ * a distinction between the view from the data model (concrete scale) and the
+ * data itself (realised scale).
+ *
+ * The terms used are:
+ * - intent: all attributes matched in this concept
+ * - extent: all objects matching all attributes in the intent
+ * - attribute contingent: all attribute matched in this concept but not in any
+ *                         upper neighbour
+ * - object contingent:    all objects matching the intent but nothing else
+ *
+ * The contingents are usually used for labelling the diagram.
+ */
+interface Concept
+{
+    /**
+     * Returns true if the concept is realised in the current context, false if
+     * it is only in the data model but not supported by the current data.
+     */
+    boolean isRealized();
+
+    /**
+     * Returns the size of the intent.
+     */
+    int getIntentSize();
+
+    /**
+     * Returns the size of the extent.
+     */
+    int getExtentSize();
+
+    /**
+     * Returns the size of the attribute contingent.
+     */
+    int getAttributeContingentSize();
+
+    /**
+     * Returns the size of the object contingent.
+     */
+    int getObjectContingentSize();
+
+    /**
+     * Returns an iterator returning the attributes from the intent.
+     *
+     * The Java objects returned from the iterator should offer a toString()
+     * implementation suited for displaying the attributes.
+     */
+    Iterator getIntentIterator();
+
+    /**
+     * Returns an iterator returning the objects from the extent.
+     *
+     * The Java objects returned from the iterator should offer a toString()
+     * implementation suited for displaying the objects.
+     */
+    Iterator getExtentIterator();
+
+    /**
+     * Returns an iterator returning the attributes from the contingent.
+     *
+     * The Java objects returned from the iterator should offer a toString()
+     * implementation suited for displaying the attributes.
+     */
+    Iterator getAttributeContingentIterator();
+
+    /**
+     * Returns an iterator returning the objects from the contingent.
+     *
+     * The Java objects returned from the iterator should offer a toString()
+     * implementation suited for displaying the objects.
+     */
+    Iterator getObjectContingentIterator();
+}
