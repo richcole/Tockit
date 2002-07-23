80912a4fac2c17d221c603c6f32eb29790553368
rj-cole <rj-cole@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2002-07-23 08:59:23 +0000

    A fix for java reference counting
    System.gc() for garbage collecting has been added. This get the
    reference count correct without have to explicitly finalize objects.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@68 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- sarl/java/TestSarl.java
+++ sarl/java/TestSarl.java
@@ -17,8 +17,7 @@ class TestSarl
     }
   }
 
-  public static void main(String argv[]) 
-  {
+  public static void test() {
     Set A = new Set();
     Set B = new Set();
 
@@ -34,30 +33,19 @@ class TestSarl
       }
     }
 
-    SetIterator tmp_it_A = new SetIterator(A);
-    SetIterator tmp_it_B = new SetIterator(B);
-    SetIterator it = tmp_it_A.iterator_meet(
-      tmp_it_B
-    );
+    SetIterator it = new SetIterator(A).iterator_meet(
+      new SetIterator(B));
     
     if ( it.count() != LEN/6 ) {
       System.out.println("Error, it.count() != " + LEN/6);
     }
 
-    // enforce finalization of objects, otherwise it might be, that we do not
-    // see the effect of finalization since the Java specification does not
-    // guarantees the finalization directly after the last reference on an
-    // object has been lost
+  }
 
-    try {
-      it.finalize();
-      A.finalize();
-      B.finalize();
-      tmp_it_A.finalize();
-      tmp_it_B.finalize();
-    } catch(Throwable any) {};
-
-
+  public static void main(String argv[]) 
+  {
+    test();
+    System.gc();
   }
   
 }
