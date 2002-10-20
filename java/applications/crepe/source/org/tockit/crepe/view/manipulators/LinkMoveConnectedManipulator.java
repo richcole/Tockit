af7708e1936b0f27f87fee2aac57a5f70ea0762c
cobbie <cobbie@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2002-10-20 17:57:03 +0000

    Upload problem solved, adding in 3 link manipulators files
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@144 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ java/applications/crepe/source/org/tockit/crepe/view/manipulators/LinkMoveConnectedManipulator.java
@@ -0,0 +1,36 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: s3805812
+ * Date: 18/09/2002
+ * Time: 17:48:06
+ * To change template for new class use
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package org.tockit.crepe.view.manipulators;
+
+import org.tockit.crepe.view.GraphView;
+import org.tockit.crepe.view.LinkView;
+import org.tockit.crepe.view.LineView;
+import org.tockit.events.EventBroker;
+import org.tockit.canvas.events.CanvasItemDraggedEvent;
+
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.awt.event.InputEvent;
+
+public class LinkMoveConnectedManipulator extends LinkMoveManipulator {
+    public LinkMoveConnectedManipulator(GraphView graphView, EventBroker eventBroker) {
+        super(graphView, eventBroker);
+    }
+
+    protected void determineItemsToMove(CanvasItemDraggedEvent dragEvent, HashSet itemsToMove, Collection lineViews, LinkView linkView) {
+
+        if ((dragEvent.getModifiers() & InputEvent.CTRL_MASK) != 0 &&
+                (dragEvent.getModifiers() & InputEvent.SHIFT_MASK) == 0) {
+            // if CTRL pressed: find all nodes/links connected
+            itemsToMove.add(linkView);
+            findConnectedViewsRecursive(itemsToMove, lineViews);
+        }
+    }
+}
---
+++ java/applications/crepe/source/org/tockit/crepe/view/manipulators/LinkMoveINCManipulator.java
@@ -0,0 +1,49 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: s3805812
+ * Date: 18/09/2002
+ * Time: 17:55:15
+ * To change template for new class use
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package org.tockit.crepe.view.manipulators;
+
+import org.tockit.crepe.view.GraphView;
+import org.tockit.crepe.view.LinkView;
+import org.tockit.crepe.view.LineView;
+import org.tockit.events.EventBroker;
+import org.tockit.canvas.events.CanvasItemDraggedEvent;
+
+import java.util.HashSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.awt.event.InputEvent;
+
+public class LinkMoveINCManipulator extends LinkMoveManipulator {
+    public LinkMoveINCManipulator(GraphView graphView, EventBroker eventBroker) {
+        super(graphView, eventBroker);
+    }
+
+    protected void determineItemsToMove(CanvasItemDraggedEvent dragEvent, HashSet itemsToMove, Collection lineViews, LinkView linkView) {
+        if ((dragEvent.getModifiers() & InputEvent.CTRL_MASK) == 0 &&
+                (dragEvent.getModifiers() & InputEvent.SHIFT_MASK) == 0) {
+
+            // if CTRL not pressed: add all directly connected nodes
+            itemsToMove.add(linkView);
+            for (Iterator iterator = lineViews.iterator(); iterator.hasNext();) {
+                LineView lineView = (LineView) iterator.next();
+                if (lineView.getConnectedLinkView() == linkView) {
+                    itemsToMove.add(lineView.getConnectedNodeView());
+                }
+            }
+            // if SHIFT not pressed: remove nodes connected to other links
+
+            for (Iterator iterator = lineViews.iterator(); iterator.hasNext();) {
+                LineView lineView = (LineView) iterator.next();
+                if (lineView.getConnectedLinkView() != linkView) {
+                    itemsToMove.remove(lineView.getConnectedNodeView());
+                }
+            }
+        }
+    }
+}
