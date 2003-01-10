27934d4917a3e453755f6542437cd84d5e826012
rj-cole <rj-cole@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-01-10 05:27:51 +0000

    Added support for lattices, continuing the development effort.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@182 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ sarl/config.h
@@ -0,0 +1,8 @@
+#ifndef SARL_CONFIG_H
+#define SARL_CONFIG_H
+
+#define SARL_NOT_IMPLEMENTED \
+  { fprintf(stderr, "Error %s:%d: Function not yet implemented.\n", \
+          __FILE__, __LINE__); };
+
+#endif
