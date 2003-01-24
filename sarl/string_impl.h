3804efe05297d906ea6f64ae9a724050da03ff08
rj-cole <rj-cole@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-01-24 08:42:59 +0000

    Adding some extra files.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@197 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ sarl/string_impl.h
@@ -0,0 +1,30 @@
+#ifndef SARL_STRING_IMPL_H
+#define SARL_STRING_IMPL_H
+
+typedef char  Sarl_Char;
+typedef short Sarl_Short;
+
+struct Sarl_String
+{
+  Sarl_RefCount ref_count;
+
+  Sarl_Char  *char_buf;
+  Sarl_Short *short_buf;
+  Sarl_Index  length;
+  Sarl_Index  capacity;
+};
+
+inline void 
+  sarl_string_init(
+    struct Sarl_String *s
+  )
+{
+  sarl_ref_count_init(&s->ref_count);
+  s->char_buf  = 0;
+  s->short_buf = 0;
+  s->length = 0;
+  s->capacity = 0;
+}
+
+
+#endif
