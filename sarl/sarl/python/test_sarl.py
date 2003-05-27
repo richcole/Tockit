39ef9561b47d1aa8eae21f9b88138115c004c318
rj-cole <rj-cole@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-05-27 07:08:08 +0000

    Recovering files
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@290 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ sarl/sarl/python/test_sarl.py
@@ -0,0 +1,74 @@
+from sarl import *
+
+def checkSequence(seq, len):
+    i=1
+    it=SetIterator(a)
+    while(not it.at_end()):
+        if i <> it.value():
+            print "Error, mismatch i=", i, ", it.value()=" + str(it.value())
+        it.next()
+        i = i + 1
+    if i <> len:
+        print "Error, mistmatch expected i=", len, ", but got i=", i
+
+a = Set()
+len=20
+
+for i in range(1,len):
+    a.insert(i)
+
+it = SetIterator(a)
+checkSequence(it, len)
+it.reset()
+checkSequence(it, len)
+
+b = Set()
+c = Set()
+
+for i in range(1,len):
+    if i % 3 == 0:
+        b.insert(i)
+    if i % 2 == 0:
+        c.insert(i)
+
+b_it = SetIterator(b)
+c_it = SetIterator(c)
+it = meet(b_it, c_it)
+
+while(not it.at_end()):
+    it.next()
+    
+if it.count_remaining() != 0:
+    print "Error, it.count_remaining() returned ", it.count_remaining()
+    
+if SetIterator(b).count_remaining() != len/3:
+    print "Error, SetIterator(b).count_remaining() returned ", \
+          SetIterator(b).count_remaining()
+
+if it.count() != len/6:
+   print "Error, it.count() returned ", it.count()
+
+x = join(SetIterator(a),SetIterator(b))
+y = meet(SetIterator(a),SetIterator(b))
+z = minus(SetIterator(a), SetIterator(b))
+
+if x.subseteq(y):
+    print "Error subset(x,y) returned true"
+
+if not y.subseteq(x):
+    print "Error subset(y,x) returned false"
+
+if not y.subseteq(x):
+    print "Error subset(y,x) returned false"
+
+if z.lexical_compare(SetIterator(a)) <= 0:
+    print "Error iterator_lexical_compare(z,SetIterator(a))"
+
+if z.copy().lexical_compare(SetIterator(b)) >= 0:
+    print "Error iterator_lexical_compare(z.copy(),SetIterator(b))"
+
+if z.lexical_compare(y) >= 0:
+    print "Error iterator_lexical_compare(z,a)"
+    
+print "Test Finished"
+
