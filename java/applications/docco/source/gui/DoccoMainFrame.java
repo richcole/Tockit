94d0a10012a640e0005d0229e5086d5af9b5ca3c
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-06-13 12:02:42 +0000

    Simplified code.
    Using an action listener instead of a keyboard listener -- much easier.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@328 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/gui/DoccoMainFrame.java
+++ java/applications/docco/source/gui/DoccoMainFrame.java
@@ -12,8 +12,6 @@ import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -57,17 +55,9 @@ public class DoccoMainFrame extends JFrame {
 		
 		this.queryField.setFocusable(true);
 		
-		this.queryField.addKeyListener(new KeyListener() {
-			public void keyTyped(KeyEvent event) {
-			}
-			public void keyPressed(KeyEvent event) {
-			}
-			public void keyReleased(KeyEvent event) {
-				if (queryField.getText().length() > 0) {
-					if (event.getKeyCode() == KeyEvent.VK_ENTER) {
-						doQuery();
-					}
-				}
+		this.queryField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				doQuery();
 			}
 		});
 		
