a15db8a2e2dada44b7a0d59f25a04a1b769b21f4
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-06-23 04:37:32 +0000

    Found a nicer way of doing the same thing.
    The check for being a leaf is now done with TreeNode.isLeaf() instead
    of making assumptions about the content.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@387 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/org/tockit/docco/gui/DoccoMainFrame.java
+++ java/applications/docco/source/org/tockit/docco/gui/DoccoMainFrame.java
@@ -379,7 +379,7 @@ public class DoccoMainFrame extends JFrame {
 			}
 			DefaultMutableTreeNode firstChild = (DefaultMutableTreeNode) childrenEnum.nextElement();
 			done = true;
-			if(!childrenEnum.hasMoreElements() && !(firstChild.getUserObject() instanceof HitReference)) { // single directory child
+			if(!childrenEnum.hasMoreElements() && !firstChild.isLeaf()) { // single directory child
 				// we need to copy references to the grandchildren first, since modification
 				// of the children breaks the enumeration
 				MutableTreeNode[] grandchildren = new MutableTreeNode[firstChild.getChildCount()];
