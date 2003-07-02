6ff0354251069b1b00acd69beef4eed0ed1923e4
nataliya <nataliya@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-07-02 03:46:43 +0000

    Introducing FileFilter implementations.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@485 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ java/applications/docco/source/org/tockit/docco/indexer/filefilter/ExtensionFileFilter.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id$
+ */
+package org.tockit.docco.indexer.filefilter;
+
+import java.io.File;
+import java.io.FileFilter;
+
+
+public class ExtensionFileFilter implements FileFilter {
+	String extensionString;
+
+	public ExtensionFileFilter (String extensionString) {
+		this.extensionString = extensionString;
+	}
+	
+	public boolean accept(File file) {
+		try {
+			String fileExtension = FileExtensionExtractor.getExtension(file.getAbsoluteFile());
+			if (fileExtension.equalsIgnoreCase(extensionString)) {
+				return true;
+			}
+		}
+		catch (NotFoundFileExtensionException e) {
+			return false;
+		}
+		return false;
+	}
+	
+	
+
+}
