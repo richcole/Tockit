14ca477b2fe812f47b5ff1158d1e3deecb41051f
nataliya <nataliya@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-07-02 03:47:47 +0000

    Moved NotFoundFileExtensionException into subpackage org.tockit.docco.indexer.filefilter from org.tockit.docco.indexer.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@486 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ java/applications/docco/source/org/tockit/docco/indexer/filefilter/NotFoundFileExtensionException.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id$
+ */
+package org.tockit.docco.indexer.filefilter;
+
+import org.tockit.docco.indexer.DocumentProcessingException;
+
+
+public class NotFoundFileExtensionException extends DocumentProcessingException {
+
+	public NotFoundFileExtensionException(String message) {
+		super(message);
+
+	}
+
+	public NotFoundFileExtensionException(String message, Throwable e) {
+		super(message, e);
+	}
+
+	public NotFoundFileExtensionException(Throwable e) {
+		super(e);
+	}
+}
