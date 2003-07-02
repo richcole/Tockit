2cd80cabda3f852c54329ecd2a87e56cb67876cb
nataliya <nataliya@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-07-02 01:30:12 +0000

    Added license info
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@483 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/org/tockit/docco/indexer/MultivalentDocumentProcessor.java
+++ java/applications/docco/source/org/tockit/docco/indexer/MultivalentDocumentProcessor.java
@@ -35,6 +35,30 @@ import multivalent.node.LeafText;
  * this parser is largerly based on tool.ExtractText code from Multivalent project
  * (http://www.cs.berkeley.edu/~phelps/Multivalent/), the only addition to the core 
  * code is ability to exract metadata.
+ * 
+ * 
+ * 
+ * Multivalent license:
+ * 
+ * Parts copyright (c) 1996 The Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Permission is hereby granted, without written agreement and without
+ * license or royalty fees, to use, copy, modify, and distribute this
+ * software and its documentation for any purpose, provided that the
+ * above copyright notice and the following two paragraphs appear in
+ * all copies of this software.
+ * 
+ * IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY FOR
+ * DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ * OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE UNIVERSITY OF
+ * CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.  THE SOFTWARE PROVIDED HEREUNDER IS
+ * ON AN "AS IS" BASIS, AND THE UNIVERSITY OF CALIFORNIA HAS NO OBLIGATION TO
+ * PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
  */
 public class MultivalentDocumentProcessor implements DocumentProcessor {
 	private URI uri;
