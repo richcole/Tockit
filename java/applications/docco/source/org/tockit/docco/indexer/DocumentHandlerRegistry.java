ec7d577f3160b66644cc9f88b19f879432316f2f
nataliya <nataliya@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-07-11 05:03:52 +0000

    Added todo
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@546 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/org/tockit/docco/indexer/DocumentHandlerRegistry.java
+++ java/applications/docco/source/org/tockit/docco/indexer/DocumentHandlerRegistry.java
@@ -56,6 +56,9 @@ public class DocumentHandlerRegistry {
 		docHandlersList.remove(docHandlerMapping);
 	}
 	
+	/**
+	 * @todo do we need this method?...
+	 */
 	public void moveMapping (int originalPositionIndex, int destinationPositionIndex) {
 		Object mapping = docHandlersList.remove(originalPositionIndex);
 		docHandlersList.add(destinationPositionIndex, mapping);
