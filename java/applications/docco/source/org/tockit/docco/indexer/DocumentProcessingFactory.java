8642b35a85019338d713b95c8e41013f2966eaee
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-07-28 06:28:21 +0000

    Path field is not tokenized anymore.
    The path_words field still is.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@632 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/org/tockit/docco/indexer/DocumentProcessingFactory.java
+++ java/applications/docco/source/org/tockit/docco/indexer/DocumentProcessingFactory.java
@@ -111,7 +111,7 @@ public class DocumentProcessingFactory {
 		}
 		
 		// @todo use paths relative to the base directory of the index
-		doc.add(Field.Text(GlobalConstants.FIELD_DOC_PATH, file.getPath()));
+		doc.add(Field.Keyword(GlobalConstants.FIELD_DOC_PATH, file.getPath()));
 		doc.add(Field.Text(GlobalConstants.FIELD_DOC_PATH_WORDS, file.getParent().replace(File.separatorChar, ' ')));
 		String fileExtension = FileExtensionExtractor.getExtension(file);
 		if (fileExtension != null) {
