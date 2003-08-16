e9e078cea8a5e236687161decfeca5749d9de2cb
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-08-16 02:39:51 +0000

    Added tests for tuple equality.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@702 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ java/libraries/relations/source/org/tockit/relations/model/tests/TupleTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright DSTC Pty.Ltd. (http://www.dstc.com), Technische Universitaet Darmstadt
+ * (http://www.tu-darmstadt.de) and the University of Queensland (http://www.uq.edu.au).
+ * Please read licence.txt in the toplevel source directory for licensing information.
+ *
+ * $Id$
+ */
+package org.tockit.relations.model.tests;
+
+import org.tockit.relations.model.Tuple;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TupleTest extends TestCase {
+    public TupleTest(String s) {
+        super(s);
+    }
+
+    public static Test suite() {
+        return new TestSuite(TupleTest.class);
+    }
+
+    public void testTupleEquality() {
+        Tuple one = new Tuple(new Object[]{});    
+        Tuple two = new Tuple(new Object[]{new Object(), "string", new Integer(5)});
+        Object object = new Object();    
+        Tuple three = new Tuple(new Object[]{object, "string", new Integer(5)});    
+        Tuple four = new Tuple(new Object[]{object, "string", new Integer(5)});
+        
+        assertTrue(one.equals(one));    
+        assertTrue(!one.equals(two));    
+        assertTrue(!one.equals(three));    
+        assertTrue(!one.equals(four));    
+
+        assertTrue(!two.equals(one));    
+        assertTrue(two.equals(two));    
+        assertTrue(!two.equals(three));    
+        assertTrue(!two.equals(four));    
+
+        assertTrue(!three.equals(one));    
+        assertTrue(!three.equals(two));    
+        assertTrue(three.equals(three));    
+        assertTrue(three.equals(four));    
+
+        assertTrue(!four.equals(one));    
+        assertTrue(!four.equals(two));    
+        assertTrue(four.equals(three));    
+        assertTrue(four.equals(four));    
+    }
+}
