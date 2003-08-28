2555f22357a1f90c76c486118bb70f3c29b316e6
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-08-28 08:28:03 +0000

    Updated the PDF document handler.
    This is based on copy and paste from the PDFbox 0.6.2 example.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@742 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/plugins/pdfbox/source/org/tockit/docco/documenthandler/PdfDocumentHandler.java
+++ java/applications/docco/plugins/pdfbox/source/org/tockit/docco/documenthandler/PdfDocumentHandler.java
@@ -7,18 +7,18 @@
  */
 package org.tockit.docco.documenthandler;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.LinkedList;
 import java.util.ArrayList;
-import java.util.List;
 
-import org.pdfbox.cos.COSDocument;
+import org.pdfbox.encryption.DecryptDocument;
+import org.pdfbox.exceptions.CryptographyException;
+import org.pdfbox.exceptions.InvalidPasswordException;
 import org.pdfbox.pdfparser.PDFParser;
 import org.pdfbox.pdmodel.PDDocument;
 import org.pdfbox.pdmodel.PDDocumentInformation;
@@ -30,60 +30,113 @@ import org.tockit.docco.indexer.DocumentSummary;
 import org.tockit.plugin.Plugin;
 
 public class PdfDocumentHandler implements DocumentHandler, Plugin {
-	
-
+	/**
+	 * Pretty much copy and paste code from the PDFbox LucenePDFDocument class.
+	 * 
+	 * @todo check if we want to add the extra bits of metadata into our model.
+	 */
 	public DocumentSummary parseDocument(URL url) throws IOException, DocumentHandlerException {
-		
-		PDFParser pdfParser = new PDFParser(url.openStream());
-		pdfParser.parse();
-		PDDocument pdfDoc = pdfParser.getPDDocument();
-		if (pdfDoc.isEncrypted()) {
-			/// @todo handle exception better here?
-			throw new DocumentHandlerException("Couldn't read document " + url.getPath() 
-							+ " because it is encrypted");
-		}
-		PDDocumentInformation info = pdfDoc.getDocumentInformation();
-		
 		DocumentSummary docSummary =  new DocumentSummary();
-		
-		docSummary.authors = getAuthors(info);
-		docSummary.contentReader = getDocumentContent(pdfParser, url);
-		docSummary.creationDate = getDate(info.getCreationDate());
-		docSummary.keywords = new ArrayList();
-		docSummary.keywords.add(info.getKeywords());
-		docSummary.modificationDate = getDate(info.getModificationDate());
-		docSummary.title = info.getTitle();
+		PDDocument pdfDocument = null;
+		try
+		{
+			PDFParser parser = new PDFParser( url.openStream() );
+			parser.parse();
+
+			pdfDocument = parser.getPDDocument();
+
+
+			if( pdfDocument.isEncrypted() )
+			{
+				DecryptDocument decryptor = new DecryptDocument( pdfDocument );
+				//Just try using the default password and move on
+				decryptor.decryptDocument( "" );
+			}
+
+			//create a tmp output stream with the size of the content.
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			OutputStreamWriter writer = new OutputStreamWriter( out );
+			PDFTextStripper stripper = new PDFTextStripper();
+			stripper.writeText( pdfDocument.getDocument(), writer );
+			writer.close();
+
+			byte[] contents = out.toByteArray();
+			docSummary.contentReader = new InputStreamReader( new ByteArrayInputStream( contents ) ); 
+
+			PDDocumentInformation info = pdfDocument.getDocumentInformation();
+			if( info.getAuthor() != null )
+			{
+				docSummary.authors = new ArrayList();
+				docSummary.authors.add(info.getAuthor());
+			}
+//			if( info.getCreationDate() != null )
+//			{
+//				Date date = info.getCreationDate().getTime();
+//				//for some reason lucene cannot handle dates before the epoch
+//				//and throws a nasty RuntimeException, so we will check and
+//				//verify that this does not happen
+//				if( date.getTime() >= 0 )
+//				{
+//					document.add(Field.Text("CreationDate", DateField.dateToString( date ) ) );
+//				}
+//			}
+//			if( info.getCreator() != null )
+//			{
+//				document.add( Field.Text( "Creator", info.getCreator() ) );
+//			}
+			if( info.getKeywords() != null )
+			{
+				docSummary.keywords = new ArrayList();
+				docSummary.keywords.add(info.getKeywords());
+			}
+			if( info.getModificationDate() != null )
+			{
+				Date date = info.getModificationDate().getTime();
+				//for some reason lucene cannot handle dates before the epoch
+				//and throws a nasty RuntimeException, so we will check and
+				//verify that this does not happen
+				if( date.getTime() >= 0 )
+				{
+					docSummary.modificationDate = date;
+				}
+			}
+//			if( info.getProducer() != null )
+//			{
+//				document.add( Field.Text( "Producer", info.getProducer() ) );
+//			}
+//			if( info.getSubject() != null )
+//			{
+//				document.add( Field.Text( "Subject", info.getSubject() ) );
+//			}
+			if( info.getTitle() != null )
+			{
+				docSummary.title = info.getTitle();
+			}
+//			if( info.getTrapped() != null )
+//			{
+//				document.add( Field.Text( "Trapped", info.getTrapped() ) );
+//			}
+
+//			int summarySize = Math.min( contents.length, 500 );
+//			// Add the summary as an UnIndexed field, so that it is stored and returned
+//			// with hit documents for display.
+//			docSummary.summary = new String( contents, 0, summarySize );
+		}
+		catch( CryptographyException e )
+		{
+			throw new IOException( "Error decrypting document(" + url + "): " + e );
+		}
+		catch( InvalidPasswordException e )
+		{
+			//they didn't suppply a password and the default of "" was wrong.
+			throw new IOException( "Error: The document(" + url + ") is encrypted and will not be indexed." );
+		}
+		finally
+		{
+			pdfDocument.close();
+		}
 		
 		return docSummary;
-	}
-
-	private Reader getDocumentContent(PDFParser pdfParser, URL url) 
-										throws IOException, DocumentHandlerException {
-		PDFTextStripper pdfToText = new PDFTextStripper();
-		StringWriter writer = new StringWriter();
-		COSDocument cosDoc = pdfParser.getDocument();
-		try {
-			pdfToText.writeText(cosDoc, writer);
-			return new StringReader(writer.toString());
-		} catch (NullPointerException e) {
-			/// @todo something seems to be dodgy with the PDFBox tool -- I get NPEs on some files (OOo?)
-			throw new DocumentHandlerException("Caught null pointer exception in PDF reader for document " + url, e);
-		}
-	}
-
-	private List getAuthors(PDDocumentInformation info) {
-		List res = new LinkedList();
-		if (info.getAuthor() != null) {
-			res.add(info.getAuthor());
-		}
-		return res;
-	}
-
-	public Date getDate(Calendar cal) {
-		if (cal != null) {
-			return cal.getTime();
-		}
-		return null;
 	}
 
 	public String getDisplayName() {
