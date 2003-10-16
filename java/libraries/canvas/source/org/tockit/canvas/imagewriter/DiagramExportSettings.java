0869e1c6890f0ea80dd6f37a2fbe0ad1074d4973
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2003-10-16 04:40:46 +0000

    Replaced the diagram export class with ToscanaJ's extended version.
    The new version uses preferences to store settings persistently and
    it also remembers the last export position.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@782 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/libraries/canvas/source/org/tockit/canvas/imagewriter/DiagramExportSettings.java
+++ java/libraries/canvas/source/org/tockit/canvas/imagewriter/DiagramExportSettings.java
@@ -7,6 +7,11 @@
  */
 package org.tockit.canvas.imagewriter;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.prefs.Preferences;
+
+
 /**
  * Stores all settings needed for creating graphic exports from a Canvas.
  *
@@ -14,6 +19,15 @@ package org.tockit.canvas.imagewriter;
  * export. The file name is not stored.
  */
 public class DiagramExportSettings {
+    private static final Preferences preferences = Preferences.userNodeForPackage(DiagramExportSettings.class);
+
+    private static final String CONFIGURATION_AUTO_MODE_ENTRY = "autoMode";
+    private static final String CONFIGURATION_IMAGE_HEIGHT_ENTRY = "imageHeight";
+    private static final String CONFIGURATION_IMAGE_WIDTH_ENTRY = "imageWidth";
+    private static final String CONFIGURATION_COMMENTS_TO_CLIPBOARD_ENTRY = "saveCommentsToClipboard";
+    private static final String CONFIGURATION_COMMENTS_TO_FILE_ENTRY = "saveCommentsToFile";
+    private static final String CONFIGURATION_LAST_EXPORT_FILE_ENTRY = "lastImageExport";
+
     /**
      * Stores the type of graphic format to be used.
      */
@@ -34,16 +48,63 @@ public class DiagramExportSettings {
      */
     private boolean autoMode;
 
-	/**
-	 * Stores if the history comments should be saved to a text file.
-	 */
-	private boolean saveCommentsToFile;
+    /**
+     * Stores if the history comments should be saved to a text file.
+     */
+    private boolean saveCommentsToFile;
 
-	/**
-	 * Stores if the history comments should be copied to a clipboard.
-	 */
-	private boolean saveCommentToClipboard;
-		
+    /**
+     * Stores if the history comments should be copied to a clipboard.
+     */
+    private boolean saveCommentsToClipboard;
+
+    /**
+     * Stores the last exported file
+     */
+    private File lastImageExportFile;
+
+    /**
+     * Initialisation constructor.
+     *
+     * If autoMode is set, the other values will be overwritten each time a
+     * diagram gets exported.
+     */
+    public DiagramExportSettings() {
+        this(null,0,0,false);
+        String lastImage = preferences.get(CONFIGURATION_LAST_EXPORT_FILE_ENTRY, null);
+        if (lastImage != null) {
+            this.format = GraphicFormatRegistry.getTypeByExtension(lastImage);
+            this.lastImageExportFile = new File(lastImage);
+        } else { 
+        //no last image file so we get the first format from the graphic format registry
+            Iterator formatIterator = GraphicFormatRegistry.getIterator();
+            if (formatIterator.hasNext()){
+                this.format = (GraphicFormat) formatIterator.next();    
+            }
+            // can't find last image, so set last image file to null.
+            this.lastImageExportFile = null; 
+        }
+        this.width = preferences.getInt(CONFIGURATION_IMAGE_WIDTH_ENTRY, 500);
+        
+        this.height = preferences.getInt(CONFIGURATION_IMAGE_HEIGHT_ENTRY, 400);
+        int exportAutoMode = preferences.getInt(CONFIGURATION_AUTO_MODE_ENTRY, 1); 
+        this.autoMode = true;
+        if( exportAutoMode == 0 ) {
+            this.autoMode = false;
+        }   
+        int saveCommentsToFileTrue = preferences.getInt(CONFIGURATION_COMMENTS_TO_FILE_ENTRY, 0);
+        this.saveCommentsToFile = false;
+        if( saveCommentsToFileTrue == 1) {
+            this.saveCommentsToFile= true;   
+        }
+        int saveCommentsToClipboardTrue = preferences.getInt(CONFIGURATION_COMMENTS_TO_CLIPBOARD_ENTRY, 0);
+        this.saveCommentsToClipboard= false;
+        if( saveCommentsToClipboardTrue == 1 ){
+            this.saveCommentsToClipboard= true;
+        }
+    }
+
+        
     /**
      * Initialisation constructor.
      *
@@ -55,8 +116,10 @@ public class DiagramExportSettings {
         this.width = width;
         this.height = height;
         this.autoMode = autoMode;
-		this.saveCommentsToFile= false;
-		this.saveCommentToClipboard= false;
+        this.saveCommentsToFile= false;
+        this.saveCommentsToClipboard= false;
+        String lastImage = preferences.get(CONFIGURATION_LAST_EXPORT_FILE_ENTRY, null);
+        this.lastImageExportFile = new File(lastImage);  
     }
 
     /**
@@ -72,6 +135,8 @@ public class DiagramExportSettings {
     public void setImageSize(int width, int height) {
         this.width = width;
         this.height = height;
+        preferences.putInt(CONFIGURATION_IMAGE_WIDTH_ENTRY, width);
+        preferences.putInt(CONFIGURATION_IMAGE_HEIGHT_ENTRY, height);
     }
 
     /**
@@ -82,6 +147,11 @@ public class DiagramExportSettings {
      */
     public void setAutoMode(boolean mode) {
         this.autoMode = mode;
+        int exportAutoMode = 1;
+        if( mode == false ) {
+            exportAutoMode = 0;
+        }
+        preferences.putInt(CONFIGURATION_AUTO_MODE_ENTRY, exportAutoMode); 
     }
     
     /**
@@ -111,7 +181,7 @@ public class DiagramExportSettings {
     public boolean usesAutoMode() {
         return this.autoMode;
     }
-	
+    
     /**
      * Debugging output.
      */
@@ -123,37 +193,62 @@ public class DiagramExportSettings {
         retVal += "]\n";
         return retVal;
     }
-	/**
-	 * Returns true if the history comments are to be saved to a file.
-	 * @return boolean
-	 */
-	public boolean getSaveCommentsToFile() {
-		return saveCommentsToFile;
-	}
+    /**
+     * Returns true if the history comments are to be saved to a file.
+     * @return boolean
+     */
+    public boolean getSaveCommentsToFile() {
+        return saveCommentsToFile;
+    }
 
-	/**
-	 * Returns true if the history comments are to be saved to the clipboard.
-	 * @return boolean
-	 */
-	public boolean getSaveCommentToClipboard() {
-		return saveCommentToClipboard;
-	}
+    /**
+     * Returns true if the history comments are to be saved to the clipboard.
+     * @return boolean
+     */
+    public boolean getSaveCommentToClipboard() {
+        return saveCommentsToClipboard;
+    }
 
-	/**
-	 * Sets whether to save the history comments to a file.
-	 * @param saveCommentsToFile The saveCommentsToFile to set
-	 */
-	public void setSaveCommentsToFile(boolean saveCommentsToFile) {
-		this.saveCommentsToFile = saveCommentsToFile;
-	}
+    /**
+     * Sets whether to save the history comments to a file.
+     * @param saveCommentsToFile The saveCommentsToFile to set
+     */
+    public void setSaveCommentsToFile(boolean saveToFile) {
+        this.saveCommentsToFile = saveToFile;
+        int saveFile = 0; //false
+        if(saveToFile == true){
+            saveFile = 1;
+        }
+        preferences.putInt(CONFIGURATION_COMMENTS_TO_FILE_ENTRY,saveFile);
+    }
 
-	/**
-	 * Sets whether to save the history comments to the system clipboard.
-	 * @param saveCommentToClipboard The saveCommentToClipboard to set
-	 */
-	public void setSaveCommentToClipboard(boolean saveCommentToClipboard) {
-		this.saveCommentToClipboard = saveCommentToClipboard;
-	}
+    /**
+     * Sets whether to save the history comments to the system clipboard.
+     * @param saveCommentToClipboard The saveCommentToClipboard to set
+     */
+    public void setSaveCommentToClipboard(boolean saveCommentToClipboard) {
+        this.saveCommentsToClipboard = saveCommentToClipboard;
+        int saveToClipboard = 0; //false
+        if(saveCommentToClipboard == true){
+            saveToClipboard = 1;
+        }
+        preferences.putInt(CONFIGURATION_COMMENTS_TO_CLIPBOARD_ENTRY, saveToClipboard);
+    }
 
+    /**
+     * Gets the last exported image File.
+     * @return File
+     */
+    public File getLastImageExportFile() {
+        return lastImageExportFile;
+    }
+
+    /**
+     * Sets the last exported image file.
+     * @param lastImageExportFile The lastImageExportFile to set
+     */
+    public void setLastImageExportFile(File lastImageExportFile) {
+        this.lastImageExportFile = lastImageExportFile;
+        preferences.put(CONFIGURATION_LAST_EXPORT_FILE_ENTRY, lastImageExportFile.getAbsolutePath());
+    }
 }
-
