08efd2fbf8eee18fd38876094018b1c5aa0d193d
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2004-01-13 21:29:16 +0000

    Removed hash set from ListSet.
    Considered too dangerous since it would break if objects are mutable. We
    accept the O(n) for the checks on list instead. Additional advantage:
    no redundant storage of reference.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@796 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/libraries/utils/source/org/tockit/util/ListSetImplementation.java
+++ java/libraries/utils/source/org/tockit/util/ListSetImplementation.java
@@ -9,31 +9,156 @@ package org.tockit.util;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.ListIterator;
 
 
 public class ListSetImplementation extends AbstractList implements ListSet {
-    private Set set;
     private List list;
     
+    private static final class UnmodifiableListSet implements ListSet {
+        private static final class UnmodifiableListIterator implements ListIterator {
+            ListIterator origIt;
+            private UnmodifiableListIterator(ListIterator i) {
+                super();
+                this.origIt = i;
+            }
+            public boolean hasNext()     {return origIt.hasNext();}
+            public Object next()         {return origIt.next();}
+            public boolean hasPrevious() {return origIt.hasPrevious();}
+            public Object previous()     {return origIt.previous();}
+            public int nextIndex()       {return origIt.nextIndex();}
+            public int previousIndex()   {return origIt.previousIndex();}
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+            public void set(Object o) {
+                throw new UnsupportedOperationException();
+            }
+            public void add(Object o) {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+        private ListSet orig;
+        
+        public UnmodifiableListSet(ListSet orig) {
+            if(orig == null) {
+                throw new NullPointerException("Wrapped ListSet must not be null");
+            }
+            this.orig = orig;
+        }
+        
+        public int size() {
+            return this.orig.size();
+        }
+
+        public void clear() {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean isEmpty() {
+            return this.orig.isEmpty();
+        }
+
+        public Object[] toArray() {
+            return this.orig.toArray();
+        }
+
+        public Object get(int index) {
+            return this.orig.get(index);
+        }
+
+        public Object remove(int index) {
+            throw new UnsupportedOperationException();
+        }
+
+        public void add(int index, Object element) {
+            throw new UnsupportedOperationException();
+        }
+
+        public int indexOf(Object o) {
+            return this.orig.indexOf(o);
+        }
+
+        public int lastIndexOf(Object o) {
+            return this.orig.lastIndexOf(o);
+        }
+
+        public boolean add(Object o) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean contains(Object o) {
+            return this.orig.contains(o);
+        }
+
+        public boolean remove(Object o) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean addAll(int index, Collection c) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean addAll(Collection c) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean containsAll(Collection c) {
+            return this.orig.containsAll(c);
+        }
+
+        public boolean removeAll(Collection c) {
+            throw new UnsupportedOperationException();
+        }
+
+        public boolean retainAll(Collection c) {
+            throw new UnsupportedOperationException();
+        }
+
+        public Iterator iterator() {
+            return new UnmodifiableListIterator(this.orig.listIterator());
+        }
+
+        public List subList(int fromIndex, int toIndex) {
+            return Collections.unmodifiableList(orig.subList(fromIndex, toIndex));
+        }
+
+        public ListIterator listIterator() {
+            return new UnmodifiableListIterator(this.orig.listIterator());
+        }
+
+        public ListIterator listIterator(int index) {
+            return new UnmodifiableListIterator(this.orig.listIterator(index));
+        }
+
+        public Object set(int index, Object element) {
+            throw new UnsupportedOperationException();
+        }
+
+        public Object[] toArray(Object[] a) {
+            return this.orig.toArray(a);
+        }
+    }
+    
     public ListSetImplementation() {
-        this.set = new HashSet();
         this.list = new ArrayList();
     }
     
     public int size() {
-        return this.set.size();
+        return this.list.size();
     }
 
     public void clear() {
-        this.set.clear();
         this.list.clear();
     }
     
     public boolean isEmpty() {
-        return this.set.isEmpty();
+        return this.list.isEmpty();
     }
 
     public Object[] toArray() {
@@ -41,10 +166,9 @@ public class ListSetImplementation extends AbstractList implements ListSet {
     }
 
     public boolean add(Object o) {
-        if(this.set.contains(o)) {
+        if(this.list.contains(o)) {
             throw new IllegalArgumentException("Can not add objects twice");
         }
-        this.set.add(o);
         this.list.add(o);
         return true;
     }
@@ -54,25 +178,24 @@ public class ListSetImplementation extends AbstractList implements ListSet {
     }
 
     public void add(int index, Object element) {
-        if(this.set.contains(element)) {
+        if(this.list.contains(element)) {
             throw new IllegalArgumentException("Can not add objects twice");
         }
-        this.set.add(element);
         this.list.add(index, element);
     }
 
     public Object remove(int index) {
-        Object element = this.list.remove(index);
-        this.set.remove(element);
-        return element;
+        return this.list.remove(index);
     }
 
     public Object set(int index, Object element) {
-        if(this.set.contains(element) && this.list.get(index) != element) {
+        if(this.list.contains(element) && this.list.get(index) != element) {
             throw new IllegalArgumentException("Can not add objects twice");
         }
-        this.set.remove(this.list.get(index));
-        this.set.add(element);
         return this.list.set(index, element);
+    }
+
+    public static ListSet unmodifiableListSet(ListSet set) {
+        return new UnmodifiableListSet(set);
     }
 }
