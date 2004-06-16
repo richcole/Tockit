36ef3ef82dc78dbae5abcee52c593bc50ac1adcb
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2004-06-16 11:08:54 +0000

    Added a new image writer for PDF based on iText.
    This one does not suffer from two FreeHEP problems:
    - rotated text works
    - Unicode characters seem to be exported ok (although not as text)
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@865 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

---
+++ java/libraries/canvas/plugins/ITextPDFImageWriter/libs/README.txt
@@ -0,0 +1,5 @@
+This folder contains libraries from other projects and parties. For 3rd party libraries a license is provided
+as a LICENSE.* file, please read these files. At the moment it contains:
+
+itext.jar: 
+  iText PDF library (version 1.02b, http://www.lowagie.com/iText/, licence in LICENSE.itext)
---
+++ java/libraries/canvas/plugins/ITextPDFImageWriter/source/org/tockit/canvas/imagewriter/ITextPDFImageWriter.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright Peter Becker (http://www.peterbecker.de). Please
+ * read licence.txt file provided with the distribution for
+ * licensing information.
+ * 
+ * $Id$
+ */
+package org.tockit.canvas.imagewriter;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import org.tockit.canvas.Canvas;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.DefaultFontMapper;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfTemplate;
+import com.lowagie.text.pdf.PdfWriter;
+
+/**
+ * Saves a Canvas as SVG graphic.
+ */
+public class ITextPDFImageWriter implements ImageWriter {
+    /**
+     * A format representing SVG.
+     */
+    static protected class GraphicFormatPDF extends GraphicFormat {
+        /**
+         * Implements GraphicFormat.getName().
+         */
+        public String getName() {
+            return "Portable Document Format (PDF)";
+        }
+
+        /**
+         * Implements GraphicFormat.getExtensions().
+         */
+        public String[] getExtensions() {
+            String[] retVal = new String[1];
+            retVal[0] = "pdf";
+            return retVal;
+        }
+
+        /**
+         * Implements GraphicFormat.getWriter().
+         */
+        public ImageWriter getWriter() {
+            return singleton;
+        }
+    }
+
+    /**
+     * The only instance of this class.
+     */
+    static private ITextPDFImageWriter singleton;
+
+    /**
+     * We use a singleton approach, no public constructor.
+     */
+    private ITextPDFImageWriter() {
+    }
+
+    /**
+     * Registers our graphic format and sets up the instance.
+     */
+    static public void initialize() {
+        singleton = new ITextPDFImageWriter();
+        GraphicFormatRegistry.registerType(new GraphicFormatPDF());
+    }
+
+    /**
+     * Saves the canvas using the settings to the file.
+     */
+    public void exportGraphic(Canvas canvas, DiagramExportSettings settings, File outputFile, Properties metadata)
+            throws ImageGenerationException {
+        canvas.getController().hideMouseFromItems(true);
+            	
+        if (settings.usesAutoMode()) {
+            // update information
+            settings.setImageSize(canvas.getWidth(), canvas.getHeight());
+        }
+    
+        try {
+			int width = settings.getImageWidth();
+            int height = settings.getImageHeight();
+
+            Document document = new Document();
+            // set size for the document, but keep existing margins
+            float docWidth = width + document.leftMargin() + document.rightMargin();
+            float docHeight = height + document.topMargin() + document.bottomMargin();
+			document.setPageSize(new Rectangle(0,docWidth,docHeight,0));
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(outputFile));
+            document.open();
+            
+            DefaultFontMapper mapper = new DefaultFontMapper();
+            PdfContentByte cb = writer.getDirectContent();
+            PdfTemplate tp = cb.createTemplate(width, height);
+            Graphics2D g2 = tp.createGraphics(width, height, mapper);
+            tp.setWidth(width);
+            tp.setHeight(height);
+
+            Rectangle2D bounds = new Rectangle2D.Double(0, 0, width, height);
+            if(settings.forceColorIsSet()) {
+                g2.setPaint(settings.getBackgroundColor());
+                g2.fill(bounds);
+            }
+            
+            AffineTransform transform = canvas.scaleToFit(g2, bounds);
+            g2.transform(transform);
+
+            canvas.paintCanvas(g2);
+
+            g2.dispose();
+            cb.addTemplate(tp, 0, 0);
+            document.close();
+        } catch (Exception e) {
+            throw new ImageGenerationException("Error while generating '" +
+                    outputFile.getPath() + "' - writing PDF error: " + e.getMessage(), e);
+		} finally {
+			canvas.getController().hideMouseFromItems(false);
+        }
+    }
+}
