8e510d1ca65574fdfed8da9d02de4839957a376f
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2005-03-06 16:55:48 +0000

    Removed superflous import.
    Slipped in when experimenting.
    
    
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@937 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/docco/source/org/tockit/docco/gui/DoccoMainFrame.java
+++ java/applications/docco/source/org/tockit/docco/gui/DoccoMainFrame.java
@@ -17,7 +17,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Rectangle;
 import java.awt.SystemColor;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
