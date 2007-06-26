69dbb2768778fb633b51e9f220cb59d82a9b79da
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2007-06-26 12:41:46 +0000

    Removed obsolete namespace for compilation units.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@1079 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/cass/exporter/source/org/tockit/cass/javaexport/Namespaces.java
+++ java/applications/cass/exporter/source/org/tockit/cass/javaexport/Namespaces.java
@@ -12,7 +12,6 @@ import java.util.Map;
 public class Namespaces {
 	public static final String CASS_TYPES = "http://tockit.org/cass/entityTypes#";
 	public static final String CASS_PROPERTIES = "http://tockit.org/cass/properties#";
-	public static final String COMPILATION_UNITS = "http://tockit.org/cass/compUnits#";
 	public static final String PACKAGES = "http://tockit.org/cass/packages#";
 	public static final String TYPES = "http://tockit.org/cass/types#";
 	public static final String METHODS = "http://tockit.org/cass/methods#";
@@ -20,7 +19,6 @@ public class Namespaces {
 	public static final Map PREFIX_MAPPING = new HashMap() {{
 		put("cassType", CASS_TYPES);
 		put("cassProp", CASS_PROPERTIES);
-		put("compUnit", COMPILATION_UNITS);
 		put("package", PACKAGES);
 		put("type", TYPES);
 		put("method", METHODS);
