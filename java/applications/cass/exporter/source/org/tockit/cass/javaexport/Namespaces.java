d9ae7be9c959bd85f783afaaa830f79f3bdd885e
peterbecker <peterbecker@f8b840a1-a80d-0410-987b-95fbfcb7d1ee>
2009-05-05 01:10:27 +0000

    Added serialVersionUID to make Eclipse happy.
    git-svn-id: svn://svn.code.sf.net/p/tockit/code/trunk@1134 f8b840a1-a80d-0410-987b-95fbfcb7d1ee

--- java/applications/cass/exporter/source/org/tockit/cass/javaexport/Namespaces.java
+++ java/applications/cass/exporter/source/org/tockit/cass/javaexport/Namespaces.java
@@ -10,24 +10,27 @@ import java.util.Map;
  * The main method prints the prefix mappings in SPARQL syntax.
  */
 public class Namespaces {
-	public static final String CASS_TYPES = "http://tockit.org/cass/entityTypes#";
-	public static final String CASS_PROPERTIES = "http://tockit.org/cass/properties#";
-	public static final String PACKAGES = "http://tockit.org/cass/packages#";
-	public static final String TYPES = "http://tockit.org/cass/types#";
-	public static final String METHODS = "http://tockit.org/cass/methods#";
-	
-	public static final Map<String,String> PREFIX_MAPPING = new HashMap<String,String>() {{
-		put("cassType", CASS_TYPES);
-		put("cassProp", CASS_PROPERTIES);
-		put("package", PACKAGES);
-		put("type", TYPES);
-		put("method", METHODS);
-	}};
-	
-	public static void main(String[] args) {
-		for (Iterator<Map.Entry<String,String>> iter = PREFIX_MAPPING.entrySet().iterator(); iter.hasNext();) {
-			Map.Entry<String,String> entry = iter.next();
-			System.out.println("PREFIX " + entry.getKey() + ": <" + entry.getValue() + ">");
-		}
-	}
+    public static final String CASS_TYPES = "http://tockit.org/cass/entityTypes#";
+    public static final String CASS_PROPERTIES = "http://tockit.org/cass/properties#";
+    public static final String PACKAGES = "http://tockit.org/cass/packages#";
+    public static final String TYPES = "http://tockit.org/cass/types#";
+    public static final String METHODS = "http://tockit.org/cass/methods#";
+
+    public static final Map<String, String> PREFIX_MAPPING = new HashMap<String, String>() {
+        private static final long serialVersionUID = 1L;
+        {
+            put("cassType", CASS_TYPES);
+            put("cassProp", CASS_PROPERTIES);
+            put("package", PACKAGES);
+            put("type", TYPES);
+            put("method", METHODS);
+        }
+    };
+
+    public static void main(String[] args) {
+        for (Iterator<Map.Entry<String, String>> iter = PREFIX_MAPPING.entrySet().iterator(); iter.hasNext();) {
+            Map.Entry<String, String> entry = iter.next();
+            System.out.println("PREFIX " + entry.getKey() + ": <" + entry.getValue() + ">");
+        }
+    }
 }
